be7884ddffa23edb71040d66d7fae180a0978253
charlie.d <charles_dong@hotmail.com>
2018-08-01 23:34:53 +0800

    add LeaveRequest and Profile models

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Question, Choice
+from .models import Question, Choice, LeaveRequest, Profile
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 # Register your models here.
 class ChoiceInline(admin.TabularInline):
@@ -17,6 +19,26 @@ class QuestionAdmin(admin.ModelAdmin):
     list_filter = ['pub_date']
     search_fields = ['question_text']
 
-admin.site.register(Question, QuestionAdmin)
+class LeaveRequestAdmin(admin.ModelAdmin):
+    list_display = ('leave_requester', 'leave_start_date', 'leave_end_date', 'leave_days', 'leave_request_status')
+    
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Leave Quota Information'
+    fk_name = 'user'
 
+class CustomizedUserAdmin(UserAdmin):
+    inlines = (ProfileInline, )
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomizedUserAdmin, self).get_inline_instances(request, obj)
+    
+
+admin.site.unregister(User)
+admin.site.register(User, CustomizedUserAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(LeaveRequest, LeaveRequestAdmin)
 
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,6 +1,9 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -25,4 +28,42 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    anual_leave_quota = models.IntegerField(default=10)
+    anual_leave_remain = models.IntegerField(default=10)
+    sick_leave_quota = models.IntegerField(default=10)
+    sick_leave_remain = models.IntegerField(default=5)
+    #supervisor = models.ForeignKey(User, on_delete=models.PROTECT)
+    
+    def __str__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
+
+class LeaveRequest(models.Model):
+    PENDING = 0
+    APPROVED = 1
+    REJECTED = 2
+    REQUEST_STATUS_CHOIES = (
+        (PENDING, 'Pending'),
+        (APPROVED, 'Approved'),
+        (REJECTED, 'Rejected')
+        )
+    leave_start_date = models.DateTimeField('leave start')
+    leave_end_date = models.DateTimeField('leave end')
+    leave_request_status = models.IntegerField(
+        choices = REQUEST_STATUS_CHOIES,
+        default = PENDING,
+        )
+    leave_requester = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def leave_days(self):
+        delta = self.leave_end_date - self.leave_start_date
+        return delta.day
 
